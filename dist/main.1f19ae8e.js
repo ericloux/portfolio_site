50fd5df38b1ab1305353c3459688c66fbc020f67
Eric Loux <eric.charles.loux@gmail.com>
2019-08-08 18:18:50 -0500

    Netfily debug

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -1744,7 +1744,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45543" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35599" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
